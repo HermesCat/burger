5f10dc36e9dfe306eafc3874f1d85087e32b91a5
HermesCat <jppettis@comcast.net>
2019-05-22 01:04:48 -0400

    initial commit

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,45 @@
+
+$(function() {
+  $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burgerinput").val().trim(),
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("Added new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+
+
+    $(".change-devour").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevour = $(this).data("newdevour");
+  
+      var newDevourState = {
+        devour: newDevour
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevourState
+      }).then(
+        function() {
+          console.log("changed devour to", newDevour);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require("express");
 
 var PORT = process.env.PORT || 8080;
@@ -7,7 +8,7 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Parse application body as JSON
+// Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
